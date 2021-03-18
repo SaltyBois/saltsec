50566403bf694fd15e90a86bfa32c2d3c27acb9b
L4v <ivosevic.jovan@uns.ac.rs>
2021-03-18 21:04:36 +0100

    :card_file_box: Added test data to database and envvar checking

--- src/main.go
+++ src/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
     "encoding/json"
+    "strconv"
     "log"
+    "errors"
     "fmt"
     "os"
     "net/http"
@@ -14,13 +16,61 @@ import (
 
 // TODO(Jovan): Use envvar for port
 const (
-    PORT        = ":8081"
-    HOST_DB     = "localhost"
-    PORT_DB     = 5432
-    USER_DB     = "postgres"
-    PASSWORD_DB = "root"
-    NAME_DB     = "SaltyData"
+    DEFAULT_PORT        = ":8081"
+    DEFAULT_HOST_DB     = "localhost"
+    DEFAULT_PORT_DB     = 5432
+    DEFAULT_USER_DB     = "postgres"
+    DEFAULT_PASSWORD_DB = "root"
+    DEFAULT_NAME_DB     = "SaltyData"
 )
+
+var (
+    PORT        string
+    HOST_DB     string
+    PORT_DB     int
+    USER_DB     string
+    PASSWORD_DB string
+    NAME_DB     string
+)
+
+// TODO(Jovan): Make it cleaner???
+func loadGlobalVars() {
+    if val, present := os.LookupEnv("PORT_SALT"); present {
+        PORT = val
+    } else {
+        PORT = DEFAULT_PORT
+    }
+
+    if val, present := os.LookupEnv("HOST_DB_SALT"); present {
+        HOST_DB = val
+    } else {
+        HOST_DB = DEFAULT_HOST_DB
+    }
+
+    if val, present := os.LookupEnv("PORT_DB_SALT"); present {
+        PORT_DB, _ = strconv.Atoi(val)
+    } else {
+        PORT_DB = DEFAULT_PORT_DB
+    }
+
+    if val, present := os.LookupEnv("USER_DB_SALT"); present {
+        USER_DB = val
+    } else {
+        USER_DB = DEFAULT_USER_DB
+    }
+
+    if val, present := os.LookupEnv("PASSWORD_DB_SALT"); present {
+        PASSWORD_DB = val
+    } else {
+        PASSWORD_DB = DEFAULT_PASSWORD_DB
+    }
+
+    if val, present := os.LookupEnv("NAME_DB_SALT"); present {
+        NAME_DB = val
+    } else {
+        NAME_DB = DEFAULT_NAME_DB
+    }
+}
 
 // TODO(Jovan): Move to common middleware?
 func jsonMiddleware(next http.Handler) http.Handler {
@@ -48,10 +98,16 @@ func connectToDb() (*gorm.DB, error) {
         return db, err
 }
 
+type DBTestStruct struct {
+    ID   uint64
+    Name string
+}
+
 func migrateData(db *gorm.DB) {
     /* TODO(Jovan): Add migrations; eg: 
     db.AutoMigrate(&package.Struct{})
     */
+    db.AutoMigrate(&DBTestStruct{})
 }
 
 func seedData(db *gorm.DB) {
@@ -66,12 +122,18 @@ func seedData(db *gorm.DB) {
         db.Create(&newData)
     }
     */
+    data := DBTestStruct{}
+    result := db.First(&data)
+    if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+        newData := DBTestStruct{ ID: 1234, Name: "Test One"}
+        db.Create(&newData)
+    }
 }
 
 func main() {
-
-    if _, dbpresent := os.LookupEnv("DB_DEV"); !dbpresent {
-        log.Println("DB_DEV set to true, not using database...")
+    loadGlobalVars()
+    if _, dbpresent := os.LookupEnv("DB_DEV"); dbpresent {
+        log.Println("DB_DEV set, using database...")
         db, err := connectToDb()
         if err != nil {
             panic("Failed to connect to database")
@@ -80,6 +142,8 @@ func main() {
         migrateData(db)
         seedData(db)
         log.Println("Successfully connected to Salty Database!")
+    } else {
+        log.Println("DB_DEV not set, not using database...")
     }
 
 
