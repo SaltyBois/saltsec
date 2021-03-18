b75540c3af2729ae882400a7e9c307af8fdbdcfa
L4v <ivosevic.jovan@uns.ac.rs>
2021-03-18 20:31:55 +0100

    :card_file_box: Reworked for GORM

--- src/go.mod
+++ src/go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.0 // indirect
 	github.com/rs/cors v1.7.0 // indirect
+	gorm.io/driver/postgres v1.0.8 // indirect
+	gorm.io/gorm v1.21.3 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-    "database/sql"
     "encoding/json"
+    "log"
     "fmt"
+    "os"
+    "net/http"
     "github.com/gorilla/mux"
     "github.com/rs/cors"
-    "log"
-    "net/http"
-    _ "github.com/lib/pq"
+    "gorm.io/gorm"
+    "gorm.io/driver/postgres"
 )
 
 // TODO(Jovan): Use envvar for port
 const (
-    PORT     = ":8081"
-    HOST     = "localhost"
-    PORT_DB  = 5432
-    USER     = "postgres"
-    PASSWORD = "root"
-    DBNAME   = "SaltyData"
+    PORT        = ":8081"
+    HOST_DB     = "localhost"
+    PORT_DB     = 5432
+    USER_DB     = "postgres"
+    PASSWORD_DB = "root"
+    NAME_DB     = "SaltyData"
 )
 
 // TODO(Jovan): Move to common middleware?
@@ -40,29 +41,53 @@ func Ping() func(http.ResponseWriter, *http.Request) {
     }
 }
 
+func connectToDb() (*gorm.DB, error) {
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d",
+        HOST_DB, USER_DB, PASSWORD_DB, NAME_DB, PORT_DB)
+        db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+        return db, err
+}
+
+func migrateData(db *gorm.DB) {
+    /* TODO(Jovan): Add migrations; eg: 
+    db.AutoMigrate(&package.Struct{})
+    */
+}
+
+func seedData(db *gorm.DB) {
+    /* TODO(Jovan): Add seeding; eg: 
+    // Check for existing data
+    data := package.Struct{}
+    result := db.First(&data)
+    if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+        newData := package.Struct{ <fill_with_data>... }
+        ....
+        // Insert into DB
+        db.Create(&newData)
+    }
+    */
+}
+
 func main() {
 
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-        HOST, PORT_DB, USER, PASSWORD, DBNAME)
+    if _, dbpresent := os.LookupEnv("DB_DEV"); !dbpresent {
+        log.Println("DB_DEV set to true, not using database...")
+        db, err := connectToDb()
+        if err != nil {
+            panic("Failed to connect to database")
+        }
 
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
-
-    err = db.Ping()
-    if err != nil {
-        panic(err)
+        migrateData(db)
+        seedData(db)
+        log.Println("Successfully connected to Salty Database!")
     }
 
-    fmt.Println("Successfully connected to Salty Database!")
 
     router := mux.NewRouter()
     router.Use(jsonMiddleware)
     router.HandleFunc("/ping", Ping()).Methods("GET")
 
     handler := cors.Default().Handler(router)
-    fmt.Printf("Starting main backend on port %s...\n", PORT)
+    log.Printf("Starting main backend on port %s...\n", PORT)
     log.Fatal(http.ListenAndServe(PORT, handler))
 }
